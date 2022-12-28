c0ee21f96fdf1c3dccaa7930a143b4dc1aed944d
Mark C. Kuzyk <mark@Marks-MacBook-Pro.local>
2022-12-28 12:08:08 -0800

    Add window navigation shortcuts

--- lua/mark/remap.lua
+++ lua/mark/remap.lua
@@ -1,4 +1,11 @@
+-- Window management
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>s", vim.cmd.split)
+vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>l", '<c-w>l')
+vim.keymap.set("n", "<leader>h", '<c-w>h')
+vim.keymap.set("n", "<leader>j", '<c-w>j')
+vim.keymap.set("n", "<leader>k", '<c-w>k')
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
