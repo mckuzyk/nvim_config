3aac4322c2127578003c2f94b8e660b4af57e95f
Mark C. Kuzyk <mark@Marks-MacBook-Pro.local>
2023-01-02 11:59:48 -0800

    Add markdown-preview

--- lua/mark/packer.lua
+++ lua/mark/packer.lua
@@ -65,4 +65,13 @@ return require('packer').startup(function(use)
   use "rebelot/kanagawa.nvim"
   use "morhetz/gruvbox"
 
+  -- Markdown previewing
+  -- install without yarn or npm
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
   end)
